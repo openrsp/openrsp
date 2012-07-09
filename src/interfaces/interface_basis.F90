openrsp/openrsp

commit db078030919e3b08c0c9096608e817188b02d2d9

remove basis from rsp_cfg

src/interfaces/interface_basis.F90
@@ -1,5 +1,7 @@
 module interface_basis
 
+   use basis_set, only: cgto
+
    implicit none
 
    public interface_basis_init
@@ -10,9 +12,28 @@ module interface_basis
 !  if false the interface will refuse to be accessed
    logical :: is_initialized = .false.
 
+   type(cgto), pointer, public :: interface_basis_pointer(:)
+
 contains
 
    subroutine interface_basis_init()
+
+      integer              :: num_cgto_blocks
+      integer              :: num_exp_and_ctr
+      real(8), allocatable :: exp_and_ctr(:)
+
+      call shells_find_sizes(num_cgto_blocks, num_exp_and_ctr)
+
+      nullify(interface_basis_pointer)
+      allocate(interface_basis_pointer(num_cgto_blocks))
+      allocate(exp_and_ctr(num_exp_and_ctr))
+
+      call shells_to_type_cgto(num_cgto_blocks, &
+                               num_exp_and_ctr, &
+                               exp_and_ctr,     &
+                               interface_basis_pointer)
+
+      deallocate(exp_and_ctr)
 
       is_initialized = .true.
 
@@ -20,8 +41,128 @@ contains
 
    subroutine interface_basis_finalize()
 
+      deallocate(interface_basis_pointer)
+      nullify(interface_basis_pointer)
+
       is_initialized = .false.
 
    end subroutine
+
+   subroutine check_if_interface_is_initialized()
+      if (.not. is_initialized) then
+         print *, 'error: you try to access interface_basis'
+         print *, '       but this interface is not initialized'
+         stop 1
+      end if
+   end subroutine
+
+  !> Count the number of contracted Gaussian-type orbital shells
+  !> \param ncgto, and number of exponents and contraction coefficents
+  !> \param nectr, so that memory for data structures can be allocated
+  subroutine SHELLS_find_sizes(ncgto, nectr)
+    implicit integer (i,m-n)
+#include <implicit.h>
+    integer, intent(out) :: ncgto, nectr
+    ! need MXSHEL
+#include <maxorb.h>
+    ! need NLRGSH NBCH NUCO NRCO
+#include <shells.h>
+    logical haveit(MXSHEL)
+    integer i, j
+    ! count the number of cgto blocks and the number of cgto
+    ! exponents (from +1 below) and contraction coefficients
+    ncgto = 0
+    nectr = 0
+    haveit(:) = .false.
+    do i = 1, NLRGSH
+       ! if not the first contacted in this block, skip
+       if (NUMCF(i) /= 1) cycle
+       ! index of AO shell block
+       j = NBCH(i)
+       if (j <= 0 .or. j > MXSHEL) &
+          call quit('SHELLS_sizes error: unexpected NBCH, <=0 or >MXSHEL')
+       ! count as cgto
+       ncgto = ncgto + 1
+       ! if first occurance of this block, count exponents and
+       ! contraction coefficients
+       if (haveit(j)) cycle
+       haveit(j) = .true.
+       nectr = nectr + NUCO(i)*(NRCO(i)+1)
+    end do
+  end subroutine
+
+  subroutine SHELLS_to_type_cgto(ncgto, nectr, ectr, bas)
+    !radovan: fixme ugly dependency on basis_set
+    use basis_set, only: cgto
+    implicit integer (i,m-n)
+#include <implicit.h>
+    integer,          intent(in)  :: ncgto, nectr
+    type(cgto),       intent(out) :: bas(ncgto)
+    real(8), target, intent(out) :: ectr(nectr)
+    ! need MXSHEL
+#include <maxorb.h>
+    ! need NLRGSH NBCH NUCO NRCO NUMCF CENT NHKT NSTRT
+#include <shells.h>
+    ! need MXCONT
+#include <aovec.h>
+    ! need PRIEXP PRICCF
+#include <primit.h>
+    ! need MAXQNM
+#include <maxmom.h>
+    integer alreadyis(MXSHEL)
+    real(8) rescal(MXQNM+1)
+    integer i, j, k, l, m, ne, nc, maxm
+    ! coefficient rescaling factors
+    maxm = 0
+    rescal(maxm+1) = 2*sqrt(acos(-1d0))
+    k = 0 !index in bas
+    l = 0 !offset in ectr
+    alreadyis(:) = 0
+    do i = 1, NLRGSH
+       if (NBCH(i) <= 0 .or. NBCH(i) > MXSHEL) &
+          call quit('SHELLS_sizes error: unexpected NBCH<=0 or >MXSHEL')
+       ! if not the first contracted in this block, skip
+       if (NUMCF(i) /= 1) cycle
+       ! index of AO shell block
+       j = NBCH(i)
+       ! fill cgto entry
+       k = k + 1
+       ne = NUCO(i) !number of exponents
+       nc = NRCO(i) !number of contracted
+       bas(k)%cent   = CENT(i,:,1)
+       bas(k)%mom    = NHKT(i) - 1
+       bas(k)%charge = huge(1) !ajt FIXME wrong
+       bas(k)%icent  = NCENT(i)
+       bas(k)%nbas   = nc * (2*bas(k)%mom + 1)
+       bas(k)%ibas   = NSTRT(i)
+       if (alreadyis(j) /= 0) then
+          bas(k)%exp => bas(alreadyis(j))%exp(:)
+          bas(k)%ctr => bas(alreadyis(j))%ctr(:,:)
+       else
+          alreadyis(j) = k
+          ! copy exponents from PRIEXP to ectr
+          j = JSTRT(i)
+          ectr(l+1:l+ne) = PRIEXP(j+1:j+ne)
+          bas(k)%exp => ectr(l+1:l+ne)
+          l = l + ne
+          ! copy and rescale contraction coefs from PRICCF to ectr
+          do while (maxm < bas(k)%mom)
+             maxm = maxm + 1
+             rescal(maxm+1) = rescal(maxm) / sqrt(2*maxm+1d0)
+          end do
+          ectr(l+1:l+ne*nc) = rescal(bas(k)%mom+1) * reshape( &
+                              transpose(PRICCF(j+1:j+ne,:nc)),(/ne*nc/))
+          call point(ectr(l+1:l+ne*nc), bas(k)%ctr)
+          l = l + ne*nc
+       end if
+    end do
+  contains
+    ! point to re-ranked array
+    subroutine point(ctr, ctr_pt)
+      real(8), target,  intent(in)  :: ctr(nc,ne)
+      real(8), pointer, intent(out) :: ctr_pt(:,:)
+      ctr_pt => ctr
+    end subroutine
+  end subroutine
 
 end module

src/openrsp.F90
@@ -48,6 +48,7 @@ module openrsp
   use interface_f77_memory
   use interface_rsp_solver
   use interface_1el
+  use interface_basis
   use rsp_functions
   use rsp_contribs, only: rsp_cfg
   use rsp_general, only: p_tuple, rsp_prop
@@ -200,11 +201,7 @@ contains
 
     ! setup response config structure
     cfg%zeromat = mat_zero_like(S)
-    call SHELLS_find_sizes(num_cgto_blocks, num_exp_and_ctr)
-    allocate(cfg%basis(num_cgto_blocks))
-    allocate(exp_and_ctr(num_exp_and_ctr))
-    call SHELLS_to_type_cgto(num_cgto_blocks, num_exp_and_ctr, &
-                             exp_and_ctr, cfg%basis)
+    call interface_basis_init()
 
     num_atoms = get_nr_atoms()
 

src/rsp_contribs.f90
@@ -18,6 +18,7 @@ module rsp_contribs
   use interface_f77_memory
   use interface_1el
   use interface_scf
+  use interface_basis
   use dalton_ifc
   use basis_set,  only: cgto
 
@@ -63,8 +64,6 @@ module rsp_contribs
      !> prototype zero matrix, of same shape and multiplicity as
      !> then density matrix
      type(matrix) :: zeromat
-     !> basis
-     type(cgto), pointer :: basis(:)
   end type
 
 
@@ -228,11 +227,9 @@ contains
 
   !> Average 2-electron integrals perturbed by fields f over the
   !> product of density matrces D1 and D2
-  subroutine rsp_twoave(mol, nf, f, c, nc, D1, D2, ave)
+  subroutine rsp_twoave(nf, f, c, nc, D1, D2, ave)
     use eri_contractions, only: ctr_arg
     use eri_basis_loops,  only: unopt_geodiff_loop
-    !> structure containing integral program settings
-    type(rsp_cfg),        intent(in)  :: mol
     !> number of fields
     integer,              intent(in)  :: nf
     !> field labels in std order
@@ -288,7 +285,7 @@ contains
        allocate(tmp(ncor,ncor,ncor,1))
        arg(1) = ctr_arg(3, -huge(1), ncor, D1, D2, &
                         rank_one_pointer(ncor**3, tmp(:,:,:,1)))
-       call unopt_geodiff_loop(mol%basis, arg)
+       call unopt_geodiff_loop(interface_basis_pointer, arg)
        ! symmetrize
        do k = 1, ncor
           do j = 1, k
@@ -311,7 +308,7 @@ contains
        ! contract FULL quartic in tmp, unsymmetrized divided by 24
        arg(1) = ctr_arg(4, -huge(1), ncor, D1, D2, &
                         rank_one_pointer(ncor**4, tmp))
-       call unopt_geodiff_loop(mol%basis, arg)
+       call unopt_geodiff_loop(interface_basis_pointer, arg)
        ! symmetrize
        do l = 1, ncor
           do k = 1, l
@@ -467,7 +464,7 @@ contains
              end if
              arg(1) = ctr_arg(2, c(1)+i + ncor * (c(2)+j-1), &
                               ncor, dens, fock(ij), null_ptr)
-             call unopt_geodiff_loop(mol%basis, arg)
+             call unopt_geodiff_loop(interface_basis_pointer, arg)
           end do
        end do
     else

src/rsp_functions.f90
@@ -72,7 +72,7 @@ contains
     gra = gra + tmp
     call print_tensor(shape(tmp), tmp, 'oneave')
     ! 2-electron contribution
-    call rsp_twoave(mol, 1, (/'GEO '/), (/1/), shape(tmp), D, D, tmp)
+    call rsp_twoave(1, (/'GEO '/), (/1/), shape(tmp), D, D, tmp)
     gra = gra + tmp/2
     call print_tensor(shape(tmp), tmp/2, 'twoave')
     ! Kohn-Sham exchange correlation average
@@ -202,11 +202,11 @@ contains
     end do
     hes = hes + temp; call print_tensor(shape(temp), temp, 'HaDb')
     ! 2-electron contribution
-    call rsp_twoave(mol, 2, (/'GEO ','GEO '/), (/1,1/), shape(temp), D, D, temp)
+    call rsp_twoave(2, (/'GEO ','GEO '/), (/1,1/), shape(temp), D, D, temp)
     hes = hes + temp/2; call print_tensor(shape(temp), temp/2, 'Gab(D)D/2')
     !
     do i = 1, size(Dg)
-       call rsp_twoave(mol, 1, (/'GEO '/), (/1/), shape(temp(:,i)), D, Dg(i), temp(:,i))
+       call rsp_twoave(1, (/'GEO '/), (/1/), shape(temp(:,i)), D, Dg(i), temp(:,i))
     end do
     hes = hes + temp; call print_tensor(shape(temp), temp, 'Ga(D)Db')
 
@@ -335,11 +335,11 @@ contains
 !   =======================
 
     tmp = 0.0d0
-    call rsp_twoave(mol, 3, (/'GEO ','GEO ','GEO '/), (/1,1,1/), shape(tmp), D, D, tmp)
+    call rsp_twoave(3, (/'GEO ','GEO ','GEO '/), (/1,1,1/), shape(tmp), D, D, tmp)
     cub = cub + tmp/2
     do i = 1, size(Dg)
        tmp = 0.0d0
-       call rsp_twoave(mol, 2, (/'GEO ','GEO '/), (/1,1/), shape(tmp(i,:,:)), &
+       call rsp_twoave(2, (/'GEO ','GEO '/), (/1,1/), shape(tmp(i,:,:)), &
                        D, Dg(i), tmp(i,:,:))
        do j = 1, size(Dg)
           do k = 1, size(Dg)
@@ -350,7 +350,7 @@ contains
        end do
        do j = 1, i
           tmp = 0.0d0
-          call rsp_twoave(mol, 1, (/'GEO '/), (/1/), shape(tmp(i,j,:)), &
+          call rsp_twoave(1, (/'GEO '/), (/1/), shape(tmp(i,j,:)), &
                           Dg(i), Dg(j), tmp(i,j,:))
           do k = 1, size(Dg)
              cub(i, j, k) = cub(i, j, k) + tmp(i, j, k)
@@ -750,14 +750,14 @@ contains
 ! MR (01/2012: THE TWOAVE CALLS SEEM TO RETURN THE CORRECT RESULTS)
 
 ! E0-TYPE CALLS
-    call rsp_twoave(mol, 4, (/'GEO ','GEO ','GEO ','GEO '/), (/1,1,1,1/), shape(tmp), D, D, tmp)
+    call rsp_twoave(4, (/'GEO ','GEO ','GEO ','GEO '/), (/1,1,1,1/), shape(tmp), D, D, tmp)
     qua = qua + tmp/2; call print_tensor(shape(tmp), tmp/2, 'Gabcd(D)D/2')
 
 ! E1-TYPE CALLS
 ! D-Dg CALLS
     do i = 1, size(Dg)
        tmp = 0.0d0
-       call rsp_twoave(mol, 3, (/'GEO ','GEO ','GEO '/), (/1,1,1/), shape(tmp(i,:,:,:)), &
+       call rsp_twoave(3, (/'GEO ','GEO ','GEO '/), (/1,1,1/), shape(tmp(i,:,:,:)), &
                        D, Dg(i), tmp(i,:,:,:))
        do j = 1, size(Dg)
           do k = 1, size(Dg)
@@ -776,7 +776,7 @@ contains
     do j = 1, size(Dg)
        do i = 1, j
           tmp = 0.0d0
-          call rsp_twoave(mol, 2, (/'GEO ','GEO '/), (/1,1/), shape(tmp(:,:,i,j)), &
+          call rsp_twoave(2, (/'GEO ','GEO '/), (/1,1/), shape(tmp(:,:,i,j)), &
                           D, Dgg(i,j), tmp(:,:,i,j))
           do k = 1, size(Dg)
              do l = 1, size(Dg)
@@ -799,7 +799,7 @@ contains
     do i = 1, size(Dg)
        do j = 1, i
           tmp = 0.0d0
-          call rsp_twoave(mol, 2, (/'GEO ','GEO '/), (/1,1/), shape(tmp(i,j,:,:)), &
+          call rsp_twoave(2, (/'GEO ','GEO '/), (/1,1/), shape(tmp(i,j,:,:)), &
                           Dg(i), Dg(j), tmp(i,j,:,:))
           do k = 1, size(Dg)
              do l = 1, size(Dg)
@@ -826,7 +826,7 @@ contains
       do j = 1, k
         do i = 1, size(Dg)
            tmp = 0.0d0
-           call rsp_twoave(mol, 1, (/'GEO '/), (/1/), shape(tmp(i,j,k,:)), &
+           call rsp_twoave(1, (/'GEO '/), (/1/), shape(tmp(i,j,k,:)), &
                          Dg(i), Dgg(j,k), tmp(i,j,k,:))
            do l = 1, size(Dg)
               qua(i, j, k, l) = qua(i, j, k, l) + tmp(i, j, k, l)
@@ -968,7 +968,7 @@ contains
     call rsp_oneave(1, (/'GEO '/), (/1/), shape(tm1), D, tm1)
     gra = gra + tm1; call print_tensor(shape(tm1), tm1, 'HgD')
     ! 2-electron contribution
-    call rsp_twoave(mol, 1, (/'GEO '/), (/1/), shape(tm1), D, D, tm1)
+    call rsp_twoave(1, (/'GEO '/), (/1/), shape(tm1), D, D, tm1)
     gra = gra + tm1/2; call print_tensor(shape(tm1), tm1/2, 'Gg(D)D/2')
     ! Kohn-Sham exchange correlation average
 !   call rsp_xcave(mol, 1, (/'GEO '/), (/1/), shape(tm1), 1, (/D/), tm1)
@@ -1059,14 +1059,14 @@ contains
 
     ! 2-electron contribution
     do i = 1, size(Df)
-       call rsp_twoave(mol, 2, (/'GEO ','GEO '/), (/1,1/), shape(tmp(:,:,i)), &
+       call rsp_twoave(2, (/'GEO ','GEO '/), (/1,1/), shape(tmp(:,:,i)), &
                        D, Df(i), tmp(:,:,i))
     end do
     Eggf = Eggf + tmp; call print_tensor(shape(tmp), tmp, 'Gab(D)Df')
     do j = 1, size(Df)
        do i = 1, size(Dg)
           tmp = 0.0d0
-          call rsp_twoave(mol, 1, (/'GEO '/), (/1/), shape(tmp(:,i,j)), &
+          call rsp_twoave(1, (/'GEO '/), (/1/), shape(tmp(:,i,j)), &
                           Dg(i), Df(j), tmp(:,i,j))
           do k = 1, size(Dg)
              Eggf(k, i, j) = Eggf(k, i, j) + tmp(k, i, j)

src/rsp_general.f90
@@ -1797,14 +1797,14 @@ dtup_ind = dtup_ind + p_tuples(j)%n_perturbations
 
        if (num_p_tuples == 1) then
 
-          call rsp_twoave(mol, p_tuples(1)%n_perturbations, p_tuples(1)%plab, &
+          call rsp_twoave(p_tuples(1)%n_perturbations, p_tuples(1)%plab, &
                           (/ (1, j = 1, p_tuples(1)%n_perturbations) /), &
                           p_tuples(1)%pdim, sdf_getdata(D, get_emptypert(), &
                           (/1/)), sdf_getdata(D, get_emptypert(), (/1/)) , contrib)
 
        elseif (num_p_tuples == 2) then
 
-          call rsp_twoave(mol, p_tuples(1)%n_perturbations, p_tuples(1)%plab, &
+          call rsp_twoave(p_tuples(1)%n_perturbations, p_tuples(1)%plab, &
                           (/ (1, j = 1, p_tuples(1)%n_perturbations) /), &
                           p_tuples(1)%pdim, dens_tuple(2), &
                           sdf_getdata(D, get_emptypert(), (/1/)) , contrib)
@@ -1816,7 +1816,7 @@ dtup_ind = dtup_ind + p_tuples(j)%n_perturbations
 ! write(*,*) 'dens tuple 3', dens_tuple(3)%elms
 ! write(*,*) 'dens tuple 3 plabs', p_tuples(3)%plab
 
-          call rsp_twoave(mol, p_tuples(1)%n_perturbations, p_tuples(1)%plab, &
+          call rsp_twoave(p_tuples(1)%n_perturbations, p_tuples(1)%plab, &
                           (/ (1, j = 1, p_tuples(1)%n_perturbations) /), &
                           p_tuples(1)%pdim, dens_tuple(2), dens_tuple(3), contrib)
 
@@ -1911,7 +1911,7 @@ dtup_ind = dtup_ind + p_tuples(j)%n_perturbations
 
        contrib = 0.0
 
-       call rsp_twoave(mol, p_tuples(1)%n_perturbations, p_tuples(1)%plab, &
+       call rsp_twoave(p_tuples(1)%n_perturbations, p_tuples(1)%plab, &
                        (/ (1, j = 1, p_tuples(1)%n_perturbations) /), p_tuples(1)%pdim, &
                        sdf_getdata(D, get_emptypert(), (/1/)) , &
                        sdf_getdata(D, get_emptypert(), (/1/)) , contrib)

commit 4a960c2a070337117019a8047d59553eefe713e7

restore basis set pointer, restores cubic and higher FF
this got broken in db078030

src/interfaces/interface_basis.F90
@@ -14,6 +14,7 @@ module interface_basis
    logical :: is_initialized = .false.
 
    type(cgto), pointer, public :: interface_basis_pointer(:)
+   real(8), allocatable :: exp_and_ctr(:)
 
 !  non-allocatables
    integer :: nr_ao
@@ -24,7 +25,6 @@ contains
 
       integer              :: num_cgto_blocks
       integer              :: num_exp_and_ctr
-      real(8), allocatable :: exp_and_ctr(:)
 
 #ifdef PRG_DALTON
 ! uses nbast
@@ -55,7 +55,8 @@ contains
                                exp_and_ctr,     &
                                interface_basis_pointer)
 
-      deallocate(exp_and_ctr)
+!     do not deallocate!!! otherwise exponents will point to nirvana
+!     deallocate(exp_and_ctr)
 
       is_initialized = .true.
 