c4ed898a85520ae4e544be7ab27d35f54ff9add8
Radovan Bast <radovan.bast@irsamc.ups-tlse.fr>
2012-07-21 13:22:27 +0200

    add file

---
+++ src/openrsp_const.F90
@@ -0,0 +1,9 @@
+module openrsp_const
+
+   implicit none
+
+!  real(8), parameter :: openrsp_const_speed_of_light = 137.0359998d0
+   ! debugging with low speed of light, later make variable!
+   real(8), parameter :: openrsp_const_speed_of_light = 10.0d0
+
+end module
