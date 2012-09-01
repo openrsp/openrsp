f2cdb632424e33d8272ff99668392387a0b58a26
Radovan Bast <radovan.bast@irsamc.ups-tlse.fr>
2012-09-02 00:40:30 +0200

    adapt to new interest core, thanks Michal!

--- src/interfaces/interface_interest.F90
+++ src/interfaces/interface_interest.F90
@@ -1035,15 +1035,12 @@ contains
       real(8), intent(in)    :: xyz(3, 4)
 
       real(8) :: f
-      integer :: nr_integrals
-
 
       f = 1.0d0
 
       !> call InteRest library routine for a given batch
       call interest_eri(f,                                                   &
                         gint,                                                &
-                        nr_integrals,                                        &
                         l(1)+1, e(1), xyz(1, 1), xyz(2, 1), xyz(3, 1), c(1), &
                         l(2)+1, e(2), xyz(1, 2), xyz(2, 2), xyz(3, 2), c(2), &
                         l(3)+1, e(3), xyz(1, 3), xyz(2, 3), xyz(3, 3), c(3), &
