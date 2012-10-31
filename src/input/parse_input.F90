cfd37db1e593435f711650cc39cdbed3e751ff58
Magnus Ringholm <ringholm@ECHINOPSIS.(none)>
2012-10-31 14:53:05 +0100

    Added Egff/DFT, Egfff/HF test cases, support for custom rsp prop.
    The input now supports a way of specifying any rsp property so as
    not to rely on hardcoded property keywords.

--- src/input/parse_input.F90
+++ src/input/parse_input.F90
@@ -279,79 +279,27 @@
 
 !     --------------------------------------------------------------------------
 
+      ! MaR: Keywords for custom property specification
 
-      if (kw_matches(word, '.FFFFFFF')) then
-         openrsp_cfg_general_fffffff = .true.
+      if (kw_matches(word, '.CUSTOM')) then
+         openrsp_cfg_general_specify = .true.
       end if
 
-      if (kw_matches(word, '.GFFFFFF')) then
-         openrsp_cfg_general_gffffff = .true.
+      if (kw_matches(word, '.SPORDR ')) then
+         call kw_read(word, openrsp_cfg_specify_order)
       end if
 
-      if (kw_matches(word, '.GGFFFFF')) then
-         openrsp_cfg_general_ggfffff = .true.
+      if (kw_matches(word, '.SPRULE ')) then
+         call kw_read(word, openrsp_cfg_specify_kn(1))
+         call kw_read(word, openrsp_cfg_specify_kn(2))
       end if
 
-      if (kw_matches(word, '.GGGFFFF')) then
-         openrsp_cfg_general_gggffff = .true.
+      if (kw_matches(word, '.SPPLAB ')) then
+         allocate(openrsp_cfg_specify_plab(openrsp_cfg_specify_order))
+         do i = 1, openrsp_cfg_specify_order
+            call kw_read(word, openrsp_cfg_specify_plab(i))
+         end do
       end if
-
-      if (kw_matches(word, '.GGGGFFF')) then
-         openrsp_cfg_general_ggggfff = .true.
-      end if
-
-      if (kw_matches(word, '.GGGGGFF')) then
-         openrsp_cfg_general_gggggff = .true.
-      end if
-
-      if (kw_matches(word, '.GGGGGGF')) then
-         openrsp_cfg_general_ggggggf = .true.
-      end if
-
-      if (kw_matches(word, '.GGGGGGG')) then
-         openrsp_cfg_general_ggggggg = .true.
-      end if
-
-!     --------------------------------------------------------------------------
-
-
-      if (kw_matches(word, '.FFFFFFFF')) then
-         openrsp_cfg_general_ffffffff = .true.
-      end if
-
-      if (kw_matches(word, '.GFFFFFFF')) then
-         openrsp_cfg_general_gfffffff = .true.
-      end if
-
-      if (kw_matches(word, '.GGFFFFFF')) then
-         openrsp_cfg_general_ggffffff = .true.
-      end if
-
-      if (kw_matches(word, '.GGGFFFFF')) then
-         openrsp_cfg_general_gggfffff = .true.
-      end if
-
-      if (kw_matches(word, '.GGGGFFFF')) then
-         openrsp_cfg_general_ggggffff = .true.
-      end if
-
-      if (kw_matches(word, '.GGGGGFFF')) then
-         openrsp_cfg_general_gggggfff = .true.
-      end if
-
-      if (kw_matches(word, '.GGGGGGFF')) then
-         openrsp_cfg_general_ggggggff = .true.
-      end if
-
-      if (kw_matches(word, '.GGGGGGGF')) then
-         openrsp_cfg_general_gggggggf = .true.
-      end if
-
-      if (kw_matches(word, '.GGGGGGGG')) then
-         openrsp_cfg_general_gggggggg = .true.
-      end if
-
-!     --------------------------------------------------------------------------
 
       call check_whether_kw_found(word, kw_section)
 
