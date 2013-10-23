c1e0cd44f9afeb86df64900238388a2871d8c482
Radovan Bast <lastname at kth.se>
2013-10-23 21:21:20 +0200

    sync testing with dirac

--- test/beholder_openrsp.py
+++ test/beholder_openrsp.py
@@ -8,7 +8,7 @@ class OpenRSPTestRun(TestRun):
     def __init__(self, _file, argv):
         TestRun.__init__(self, _file, argv)
 
-    def execute(self, inp_files, mol_files, f=None, args=''):
+    def execute(self, inp_files, mol_files, f=None, args='', accepted_errors=[]):
         launcher = os.path.join(self.binary_dir, 'dalton')
         launcher += ' -noarch -nobackup'
         if args != '':
@@ -18,13 +18,11 @@ class OpenRSPTestRun(TestRun):
             for mol in mol_files:
                 mol_no_suffix = os.path.splitext(mol)[0]
                 output_no_suffix = '%s_%s' % (inp_no_suffix, mol_no_suffix)
-                sys.stdout.write('\nrunning test %s %s\n' % (inp_no_suffix, mol_no_suffix))
+                sys.stdout.write('\nrunning test: %s %s\n' % (inp_no_suffix, mol_no_suffix))
                 TestRun.execute(self,
                                 command = launcher + ' %s %s' % (inp, mol),
-                                success_message = 'Total wall time used in DALTON',
-                                success_file_name = '%s.out' % output_no_suffix,
                                 stdout_file_name = '%s.stdout' % output_no_suffix,
-                                stderr_means_crash = False)
+                                accepted_errors = accepted_errors)
                 if f != None:
                     # f is a suffix-filter dictionary
                     for suffix in f:
