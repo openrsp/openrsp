60c9f8b7cc8c6f83aa5fc7ee90ed148a908e09f6
Jógvan Magnus Haugaard Olsen <foeroyingur@gmail.com>
2013-11-08 18:20:44 +0100

    Add PE contribution to initial Fock matrix.

--- src/interfaces/interface_pelib.f90
+++ src/interfaces/interface_pelib.f90
@@ -1,20 +1,43 @@
-module interface_pe
+module interface_pelib
 
     use matrix_defop
     use matrix_lowlevel
+    use polarizable_embedding, only: pe_master
 
     implicit none
 
     private
 
-    public :: pe_rsp
+    public :: pe_rsp, pe_add_full_operator
 
 contains
 
-subroutine pe_rsp(dens, fock, nr_ao, propsize)
+subroutine pe_add_full_operator(dmat, fmat, energy)
 
-!    use pe_variables, only: peqm
-    use polarizable_embedding, only: pe_master
+    real(8), dimension(:), intent(in) :: dmat
+    real(8), dimension(:), intent(out) :: fmat
+    real(8), dimension(:), intent(out) :: energy
+
+    integer :: ndim
+    real(8), dimension(1) :: temp
+    real(8), dimension(:), allocatable :: packed_pe_fmat, packed_dmat
+
+    fmat = 0.0d0
+    energy = 0.0d0
+
+    ndim = int(sqrt(real(size(dmat))))
+    allocate(packed_pe_fmat(ndim * (ndim + 1) / 2))
+    allocate(packed_dmat(ndim * (ndim + 1) / 2))
+    packed_pe_fmat = 0.0d0
+    packed_dmat = 0.0d0
+    call dgetsp(ndim, dmat, packed_dmat)
+    call pe_master('fock', packed_dmat, packed_pe_fmat, 1, energy, temp)
+    call dsptge(ndim, packed_pe_fmat, fmat)
+    deallocate(packed_pe_fmat, packed_dmat)
+
+end subroutine pe_add_full_operator
+
+subroutine pe_rsp(dens, fock, nr_ao, propsize)
 
     !> number of fields
     integer, intent(in) :: propsize, nr_ao
@@ -51,7 +74,7 @@ subroutine pe_rsp(dens, fock, nr_ao, propsize)
     call dsptge(nr_ao, fmat_packed, fmat_full)
 
     do i = 1, propsize
-       fock(i)%elms(:,:,1) = fock(i)%elms(:,:,1) + 2.0d0*fmat_full(:,:)
+       fock(i)%elms(:,:,1) = fock(i)%elms(:,:,1) + fmat_full(:,:)
     enddo
 
     deallocate(fmat_full)
@@ -59,4 +82,4 @@ subroutine pe_rsp(dens, fock, nr_ao, propsize)
 
 end subroutine pe_rsp
 
-end module interface_pe
+end module interface_pelib
