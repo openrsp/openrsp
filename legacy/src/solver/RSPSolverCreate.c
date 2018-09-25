717e4db6257ccd7b22c0d3bde91635ac46b20646
Radovan Bast <bast@users.noreply.github.com>
2018-09-25 16:01:54 +0200

    replace license headers with 2.1 versions, less verbosity

--- legacy/src/solver/RSPSolverCreate.c
+++ legacy/src/solver/RSPSolverCreate.c
@@ -6,19 +6,10 @@
                   Magnus Ringholm,
                   Kenneth Ruud,
                   Andreas Thorvaldsen
-
-   OpenRSP is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Lesser General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   OpenRSP is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   GNU Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with OpenRSP. If not, see <http://www.gnu.org/licenses/>.
+  This source code form is subject to the terms of the
+  GNU Lesser General Public License, version 2.1.
+  If a copy of the GNU LGPL v2.1 was not distributed with this
+  code, you can obtain one at https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html.
 
    This file implements the function RSPSolverCreate().
 
